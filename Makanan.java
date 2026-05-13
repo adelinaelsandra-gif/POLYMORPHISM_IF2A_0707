7a322b6e42d4ffe9d6c0a2f81c2ea0f76dbe26c1
adelinaelsandra-gif <adelinaelsandra@gmail.com>
2026-05-13 10:15:08 +0700

    Menambahka constructor pada class Makanan

--- Makanan.java
+++ Makanan.java
@@ -1,3 +1,10 @@
-public class Makanan {
+public class Makanan extends Produk{
+    private int kadaluarsa;
+
+    public Makanan(String nama, double harga, int stok, int kadaluarsa){
+        super(nama, harga, stok);
+        this. kadaluarsa = kadaluarsa;
+    }
+    
     
 }
