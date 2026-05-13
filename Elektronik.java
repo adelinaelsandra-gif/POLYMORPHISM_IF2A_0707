87e6d5bbd3edaafb473c624ae826a6cf41a247db
adelinaelsandra-gif <adelinaelsandra@gmail.com>
2026-05-13 10:30:38 +0700

    Override method tampilInfo pada class Elektronik

--- Elektronik.java
+++ Elektronik.java
@@ -12,5 +12,8 @@ public class Elektronik extends Produk {
         return hitungTotalHarga(jmlhBeli) * 0.85;
     }
     @Override
-    
+        public void tampilInfo(){
+            super.tampilInfo();
+            System.out.println("Masa Garansi " + masaGaransi + "Tahun");
+        }
 }
