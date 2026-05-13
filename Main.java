0a4f6d74e2187957f53d02ec008f78189120cc99
adelinaelsandra-gif <adelinaelsandra@gmail.com>
2026-05-13 10:02:48 +0700

    penambahan class

--- Main.java
+++ Main.java
@@ -1,3 +1,25 @@
 public class Main {
-    
+    public static void main(String[] args) {
+        Produk p1 = new Elektronik("Samsung", 100000, 5, 2);
+        Produk p2 = new Makanan("Bakso", 1000, 100, 3);
+    }
+    public class Main {
+    public static void main(String[] args) {
+        Produk p1 = new Elektronik("Samsung", 100000, 5, 2);
+        Produk p2 = new Makanan("Bakso", 1000, 100, 3);
+
+        Kasir kasir = new Kasir("Maman");
+
+        p1.tampilInfo();
+        kasir.tampilKndentitas();
+        kasir.prosesBelanja(p1, 3);
+    }
+    System.out.println();
+        System.out.println();
+
+        p2.tampilInfo();
+        kasir.tampilKndentitas();
+        kasir.prosesBelanja(p2, 11);
 }
+}
+
--- Produk.java
+++ Produk.java
@@ -1,3 +1,5 @@
-public class Produk {
-    
+public abstract class Produk {
+    private String nama;
+    private double harga;
+    private int stok;
 }
