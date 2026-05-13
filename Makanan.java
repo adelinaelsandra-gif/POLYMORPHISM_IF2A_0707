35d24ed7d2aed55f18ac851fffbbe8dbdd02ab6f
adelinaelsandra-gif <adelinaelsandra@gmail.com>
2026-05-13 10:21:53 +0700

    add tampilInfo

--- Makanan.java
+++ Makanan.java
@@ -11,5 +11,10 @@ public class Makanan extends Produk{
         return hitungTotalHarga(jmlhBeli) * 0.95;
     
     }
-    
-}
+    @Override
+    public void tampilInfo(){
+        super.tampilInfo();
+        System.out.println("Masa Kadaluarsa: " + kadaluarsa + " Hari");
+    }
+    }
+
