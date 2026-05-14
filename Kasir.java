2aea193d23c205aa6f53e93e96999c54e37841ab
adelinaelsandra-gif <adelinaelsandra@gmail.com>
2026-05-14 07:01:09 +0700

    membuat class

--- Kasir.java
+++ Kasir.java
@@ -1,3 +1,10 @@
 public class Kasir {
-    
+    private String namaKasir;
+    public Kasir(String namaKasir){
+        this.namaKasir = namaKasir;
+    }
+    public void
+     tampilIdentitas(){
+        
+     }
 }
