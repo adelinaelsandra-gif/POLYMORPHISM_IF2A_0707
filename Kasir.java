12fd4585029c1383c4bae2c39b599933e1a05137
adelinaelsandra-gif <adelinaelsandra@gmail.com>
2026-05-14 07:10:59 +0700

    Menambahkan perhitungan harga akhir dan update stok produk

--- Kasir.java
+++ Kasir.java
@@ -14,6 +14,10 @@ public class Kasir {
         if(p.getStok() < jmlhBeli){
             System.out.println("Out of stok");
         }
-        
-     }
+        double hargaAKhir = p.hitungHargaAkhir(jmlhBeli);
+        System.out.println("Harga Akhir = " + hargaAKhir);
+        System.out.println("Harga Akhir = " + hargaAKhir);
+        p.setStok(jmlhBeli);
+     }  
+     System.out.println("Stok terbaru= " + p.getStok());
 }
